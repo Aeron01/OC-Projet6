9054827395bf4effccd2dc598766d10657be386e
Aeron01 <one.soulreaper@gmail.com>
2023-07-10 13:50:54 +0200

    removing blank space

--- script/main.js
+++ script/main.js
@@ -4,12 +4,12 @@ import { loadCategoriesEventListener } from "./filter.js";
 import { initEdition, enableEdition } from "./edition.js";
 import { loadCategories, loadData } from "./dataapi.js";
 
-window.onload = async ()=>{
+window.onload = async () => {
     initToken();
     loadHeader();
     loadFooter();
     createNavigation();
-    const[cats, data] = await Promise.all( [loadCategories(), loadData()]);
+    const[cats, data] = await Promise.all([loadCategories(), loadData()]);
     createCards(data);
     createFilters(cats);
     loadCategoriesEventListener();
